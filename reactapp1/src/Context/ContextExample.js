952b0caeca452adb49618db85cbaeaca25f987e4
Sumit <sumit555dongare@gmail.com>
2024-07-06 22:45:13 +0530

    use of context and its example code

--- reactapp1/src/Context/ContextExample.js
+++ reactapp1/src/Context/ContextExample.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+
+const MyContext = React.createContext();
 
 export default function ContextExample() {
     const [theme, setTheme] = useState('light')
@@ -7,28 +9,35 @@ export default function ContextExample() {
     <div>
         Parent Component : {theme}
 
-        <ChildComponent theme={theme}></ChildComponent>
+     <MyContext.Provider value={theme}>
+        <ChildComponent ></ChildComponent>
+     </MyContext.Provider>
+
+     <button onClick={()=>{
+        setTheme(theme==='light' ? 'dark' : 'light')
+     }}>Change Theme</button>
       
     </div>
   )
 }
 
-function ChildComponent(props){
+function ChildComponent(){
 
     return (
         <div>
-            Child : {props.theme}
-            <GrandChildComponent theme={props.theme}></GrandChildComponent>
+            Child
+            <GrandChildComponent ></GrandChildComponent>
         </div>
     )
 
 }
 
-function GrandChildComponent(props){
+function GrandChildComponent(){
+  const theme = useContext(MyContext)
 
     return (
         <div>
-            GrandChild : {props.theme}
+            Grand : {theme}
         </div>
     )
 
