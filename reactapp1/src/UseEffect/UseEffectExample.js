ae77430e4aa1c277e54314bfbfd0a2fc5936584f
Sumit <sumit555dongare@gmail.com>
2024-07-07 17:14:52 +0530

    working of Cleanup function in useEffect

--- reactapp1/src/UseEffect/UseEffectExample.js
+++ reactapp1/src/UseEffect/UseEffectExample.js
@@ -8,28 +8,40 @@ export default function UseEffectExample({name}) {
   // useEffect(()=>{}, [])
 
   //Example 1 : Without dependency array : This effect is executed on every component render.
-  useEffect(() => {
-    console.log(" 1. This is executed on every render");
-  });
+//   useEffect(() => {
+//     console.log(" 1. This is executed on every render");
+//   });
 
   //Example 2 : With empty dependency array : 
    useEffect(()=>{
     console.log(" 2. This is executed only once when component is renderd.")
+      //API Call
    }, [])
 
   //Example 3 : With dependency array : 
    useEffect(()=>{
-    console.log(" 3. This is executed when counter is updated.")
+    console.log(" 3. This is executed when counter is updated." , counter)
+
+     // Cleanup function
+    return ()=>{
+        console.log('I am cleaning up', counter)
+    }
    }, [counter])
 
   //Example 4 : With dependency array : 
    useEffect(()=>{
     console.log(" 4. This is executed when counter or counter2 is updated.")
+    return ()=>{
+        console.log('I am cleaning up')
+    }
    }, [counter, counter2])
 
   //Example 5 : With dependency array, prop  : 
    useEffect(()=>{
     console.log(" 5. This is executed when name prop is changed.")
+    return ()=>{
+        console.log('I am cleaning up')
+    }
    }, [name])
 
   return <div>
